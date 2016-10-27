e047d134c825e8d9f8b834c01a47c2bfd4a984b7
Carlos Delgado <carlos.delgado.andres@gmail.com>
2016-10-27 11:28:14 +0200

    Esquema de la tabla News

---
+++ tables/News.js
@@ -0,0 +1,35 @@
+/**
+ * Created by carlos on 27/10/16.
+ */
+
+// Dependencias
+var azureMobileApps = require("azure-mobile-apps");
+
+// Crear la tabla
+var table = azureMobileApps.table();
+
+// Esquema de la tabla
+// (tipos permitidos: string, number, date, boolean)
+table.columns = {
+    "title": "string",
+    "text": "string",
+    "image": "string",
+    "writer": "string",
+    "latitude": "number",
+    "longitude": "number",
+    "visits": "number"
+};
+
+// El esquema será estático
+table.dynamicSchema = false;
+
+// Permisos de acceso a la tabla
+// (a escoger entre anonymous, authenticated y disabled)
+table.read.access = 'anonymous';
+table.update.access = 'authenticated';
+table.delete.access = 'authenticated';
+table.insert.access = 'authenticated';
+
+
+// Exportar la tabla
+module.exports = table;
