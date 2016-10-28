ed840072844fb8d20c8b4e56465e4c02a4ec888d
Carlos Delgado <carlos.delgado.andres@gmail.com>
2016-10-28 04:49:12 +0200

    Añadida API read_news

---
+++ api/read_news.js
@@ -0,0 +1,43 @@
+/**
+ * Created by carlos on 28/10/16.
+ */
+
+var api = {
+
+    // Método GET de la api:
+    // buscar la noticia publicada con el id indicado en la tabla News
+
+    get: function(req, res, next) {
+
+        // Comprobar si la petición trae parámetros
+        if (typeof req.params.length < 0) {
+            return next();
+        }
+
+        // Obtención del id a buscar
+        var newsId = req.query.newsId;
+
+        // Obtención del contexto (el objeto que encapsula el servicio de Azure Mobile)
+        var context = req.azureMobile;
+
+        // Conexión a la BBDD del servicio
+        var database = context.data;
+
+        // Query de SQL
+        var query = {   sql: "SELECT title, writer, updatedAt, visits, image, latitude, longitude, text FROM News WHERE (id = 'newsId' AND status = 'published')"    };
+
+        // Ejecutar la query y devolver los resultados en un json
+        database.execute(query).then( function(result) {
+            res.json(result);
+        });
+    }
+
+};
+
+
+// Niveles de autenticación requeridos por esta api
+api.get.access = 'anonymous';
+
+
+// Exportar la api
+module.exports = api;
